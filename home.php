802314725e211fd2524a189b3de89660156f33c3
Alan Renteria <arenteriaperulles@outlook.com>
2016-12-09 03:40:20 -0800

    added styles to the home page

--- home.php
+++ home.php
@@ -7,13 +7,14 @@
     <meta property="og:url" content="https://www.proyectoxocolate.com/"/>
     <meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/og-image-logo.jpg"/>
     <meta property="og:site_name" content="Proyecto Chocolate"/>
-    <meta property="fb:admins" content="USER_ID"/>
+    <meta property="fb:admins" content="alan.renteria.10"/>
     <meta property="og:description"
           content="Proyecto Chocolate desarollo de chocolate artesanal "/>
     <style media="screen">
 
       body{
-        background-color:#3A281B;
+        /*background-color:#3A281B;*/
+        background-color:#594530;
       }
     </style>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -25,10 +26,18 @@
   <body>
     <script src="<?php bloginfo('template_url'); ?>/js/parallax.min.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/javascript.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/snowstorm.js"></script>
+    <script>
+snowStorm.snowColor = '#99ccff';   // blue-ish snow!?
+snowStorm.flakesMaxActive = 15;    // show more snow on screen at once
+snowStorm.useTwinkleEffect = true; // let the snow flicker in and out of view
+</script>
 
 <div id="scene">
 
-<div class="layer" data-depth="0.60">  <img  class="parallax-logo" src="<?php bloginfo('template_url'); ?>/images/px-logo.png" /></div>
+  <div class="layer" data-depth="0.05" id="background-layer" > <div id="backgroung-cover-image" style="background:url(<?php bloginfo('template_url'); ?>/images/background-image.png) no-repeat ;
+"> </div> </div>
+  <div class="layer" data-depth="0.40">  <img  class="parallax-logo" src="<?php bloginfo('template_url'); ?>/images/px-logo.png" /></div>
 
 </div>
 
