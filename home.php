376b0b668e7bd8a231d497895bc068f2aee7f4af
Alan Renteria <arenteriaperulles@outlook.com>
2016-12-27 16:07:32 -0800

    nueva pagina home

--- home.php
+++ home.php
@@ -1,45 +1,106 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title> Proyecto Xocolate</title>
-    <meta property="og:title" content="Proyecto Chocolate"/>
-    <meta property="og:url" content="https://www.proyectoxocolate.com/"/>
-    <meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/og-image-logo.jpg"/>
-    <meta property="og:site_name" content="Proyecto Chocolate"/>
-    <meta property="fb:admins" content="alan.renteria.10"/>
-    <meta property="og:description"
-          content="Proyecto Chocolate desarollo de chocolate artesanal "/>
-    <style media="screen">
+<head>
+  <meta charset="utf-8">
+  <title> Proyecto Xocolate</title>
+  <meta property="og:title" content="Proyecto Chocolate"/>
+  <meta property="og:url" content="https://www.proyectoxocolate.com/"/>
+  <meta property="og:image" content="<?php bloginfo('template_url'); ?>/images/og-image-logo.jpg"/>
+  <meta property="og:site_name" content="Proyecto Chocolate"/>
+  <meta property="fb:admins" content="alan.renteria.10"/>
+  <meta property="og:description" content="Proyecto Chocolate desarollo de chocolate artesanal "/>
+  <style media="screen">
 
-      body{
-        /*background-color:#3A281B;*/
-        background-color:#594530;
-      }
-    </style>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+          <script src="<?php bloginfo('template_url'); ?>/js/javascript.js"></script>
+          <link rel="stylesheet" type="text/css"  href="<?php bloginfo('template_url'); ?>/style.css">
+          <script src="<?php bloginfo('template_url'); ?>/js/parallax.min.js"></script>
+          <script src="<?php bloginfo('template_url'); ?>/js/javascript.js"></script>
+          <script src="<?php bloginfo('template_url'); ?>/js/snowstorm.js"></script>
+          </head>
+<body>
 
-
-            <script src="<?php bloginfo('template_url'); ?>/js/javascript.js"></script>
-            <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css">
-  </head>
-  <body>
-    <script src="<?php bloginfo('template_url'); ?>/js/parallax.min.js"></script>
-    <script src="<?php bloginfo('template_url'); ?>/js/javascript.js"></script>
-    <script src="<?php bloginfo('template_url'); ?>/js/snowstorm.js"></script>
-    <script>
-snowStorm.snowColor = '#99ccff';   // blue-ish snow!?
+  <script>
+snowStorm.snowColor = '#ffffff';   // blue-ish snow!?
 snowStorm.flakesMaxActive = 15;    // show more snow on screen at once
 snowStorm.useTwinkleEffect = true; // let the snow flicker in and out of view
 </script>
 
-<div id="scene">
+<!-- <div id="scene">
 
-  <div class="layer" data-depth="0.05" id="background-layer" > <div id="backgroung-cover-image" style="background:url(<?php bloginfo('template_url'); ?>/images/background-image.png) no-repeat ;
+<div class="layer" data-depth="0.05" id="background-layer" > <div id="backgroung-cover-image" style="background:url(<?php bloginfo('template_url'); ?>/images/background-image.png) no-repeat ;
 "> </div> </div>
-  <div class="layer" data-depth="0.40">  <img  class="parallax-logo" src="<?php bloginfo('template_url'); ?>/images/px-logo.png" /></div>
+<div class="layer" data-depth="0.40">  <img  class="parallax-logo" src="<?php bloginfo('template_url'); ?>/images/px-logo.png" /></div>
 
+</div> -->
+
+<div class="cuerpo-tienda" >
+  <img src="<?php bloginfo('template_url'); ?>/images/background-image.png" alt=""  data-depth="0.40" class="background-cover-image layer">
+<!-- <div class="background-cover-image layer" data-depth="0.40">
+</div> -->
+ <div class="first-half">
+   <div id="scene">
+     <div class="layer image" data-depth="0.10">  <img  class="parallax-logo" src="<?php bloginfo('template_url'); ?>/images/px-logo.png" />
+        </div>
+        <!-- <div class="layer text" data-depth="0.50">
+          <div >
+            <span> Proudcto 100% </span> <span> Mexicano </span>
+          </div>
+        </div> -->
+   </div>
+   <div id="scene2">
+        <div class="layer text" data-depth="0.50">
+          <div >
+            <span> Proudcto 100% </span> <span> Mexicano </span>
+          </div>
+        </div>
+   </div>
+ </div>
+ <div class="second-half">
+    <div class="fake-line">
+
+    </div>
+   <div class='cuerpo'>
+     <div class="title">
+        <div class="">
+          <span>Pagina Bajo Construccion!</span>
+        </div>
+        <div class="">
+          <span>Por el momento connectate con nosotros de las siguiente maneras!</span>
+        </div>
+     </div>
+     <div class="body">
+       <div class="body-phone">
+         <div>
+           <img src="<?php bloginfo('template_url'); ?>/images/phone.png" alt="">
+    <p>(686) 262-7198 - Jesus Renteria<br><br>(686) 248-7506 - Alan Renteria</p>
+
+         </div>
+</div>
+       <div class="body-email">
+              <img src="<?php bloginfo('template_url'); ?>/images/email.png" alt="">
+   <span>ventas@proyectoxocolate.com</span>
+ </div>
+       <div class="body-register">
+              <img src="<?php bloginfo('template_url'); ?>/images/register.png" alt="">
+         <div class="text">
+           <div>
+             <span> Obtén lo ultimo </span>
+           </div>
+           <span>Manténgase al día con las últimas noticias, eventos y ofertas
+especiales de Proyecto Xocolate.</span>
+<div class="register-box">
+      <?php echo do_shortcode( '[mc4wp_form id="8"]' ); ?>
+</div>
+         </div>
+       </div>
+   </div>
+   </div>
+ </div>
 </div>
 
-  </body>
+
+
+</body>
 </html>
--- js/javascript.js
+++ js/javascript.js
@@ -2,4 +2,8 @@
 $(document).ready(function () {
   var scene = document.getElementById('scene');
   var parallax = new Parallax(scene);
+
+  var scene2 = document.getElementById('scene2');
+  var parallax2 = new Parallax(scene2);
+
 });
