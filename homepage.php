21e25710e44187f05a835bba7922c3b2b40b62b0
Beatrenger <alanrenteria@proyectoxocolate.com>
2017-02-15 02:43:06 -0800

    fixed image issue on homepage

--- homepage.php
+++ homepage.php
@@ -5,7 +5,7 @@
 <div class="topheader">
   <div class="container">
     <div>
-      <img src="<?php bloginfo('template_url'); ?>/images/logofrontpage.png" alt="">
+      <img  src="<?php bloginfo('template_url'); ?>/images/logofrontpage.png" alt="">
     </div>
     <div class="button-menu">
       <img src="<?php bloginfo('template_url'); ?>/images/flecha-scroll-down.png" alt="">
